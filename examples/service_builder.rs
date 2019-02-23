880e90e7e1b48eb2f18640738c908e310e74acae
Lucio Franco <luciofranco14@gmail.com>
2019-02-23 12:35:20 -0500

    WIP

--- examples/service_builder.rs
+++ examples/service_builder.rs
@@ -1,42 +1,43 @@
-extern crate futures;
-extern crate http;
-extern crate hyper;
-extern crate tower;
-extern crate tower_hyper;
-extern crate tower_in_flight_limit;
-extern crate tower_rate_limit;
-extern crate tower_util;
+// extern crate futures;
+// extern crate http;
+// extern crate hyper;
+// extern crate tower;
+// extern crate tower_hyper;
+// extern crate tower_in_flight_limit;
+// extern crate tower_rate_limit;
+// extern crate tower_util;
 
-use futures::Future;
-use http::Uri;
-use hyper::client::connect::Destination;
-use hyper::client::HttpConnector;
-use hyper::{Body, Request, Response};
-use std::time::Duration;
-use tower::ServiceBuilder;
-use tower_hyper::client::{Builder, Connect};
-use tower_hyper::util::Connector;
-use tower_in_flight_limit::InFlightLimitMiddleware;
-use tower_rate_limit::RateLimitMiddleware;
+// use futures::Future;
+// use http::Uri;
+// use hyper::client::connect::Destination;
+// use hyper::client::HttpConnector;
+// use hyper::{Body, Request, Response};
+// use std::time::Duration;
+// use tower::ServiceBuilder;
+// use tower_hyper::client::{Builder, Connect};
+// use tower_hyper::util::Connector;
+// use tower_in_flight_limit::InFlightLimitMiddleware;
+// use tower_rate_limit::RateLimitMiddleware;
 
-fn main() {
-    hyper::rt::run(request.map(|_| ()).map_err(|_| ()))
-}
+// fn main() {
+//     hyper::rt::run(request.map(|_| ()).map_err(|_| ()))
+// }
 
-fn request() -> impl Future<Item = Response<Body>, Error = hyper::Error> {
-    let dst = Destination::try_from_uri(Uri::from_static("http://127.0.0.1:3000")).unwrap();
-    let connector = Connector::new(HttpConnector::new(1));
-    let hyper = Connect::new(connector, Builder::new());
+// fn request() -> impl Future<Item = Response<Body>, Error = hyper::Error> {
+//     let dst = Destination::try_from_uri(Uri::from_static("http://127.0.0.1:3000")).unwrap();
+//     let connector = Connector::new(HttpConnector::new(1));
+//     let hyper = Connect::new(connector, Builder::new());
 
-    let client = ServiceBuilder::new(hyper)
-        .middleware(InFlightLimitMiddleware::new(5))
-        .middleware(RateLimitMiddleware::new(5, Duration::from_secs(1)))
-        .build(dst);
+//     let client = ServiceBuilder::new(hyper)
+//         .middleware(InFlightLimitMiddleware::new(5))
+//         .middleware(RateLimitMiddleware::new(5, Duration::from_secs(1)))
+//         .build(dst);
 
-    let request = Request::builder()
-        .method("GET")
-        .body(Body::empty())
-        .unwrap();
+//     let request = Request::builder()
+//         .method("GET")
+//         .body(Body::empty())
+//         .unwrap();
 
-    client.call(request)
-}
+//     client.call(request)
+// }
+fn main() {}
